9c4437cc3eec2256d9956f5872d778893c4db966
Jeremias Ponciano Da Silva <jeremiason2911@gmail.com>
2026-03-23 11:06:50 -0300

    arquivos de veiculo

---
+++ src/main/java/com/youtan/leilao/DTO/VeiculoDTO.java
@@ -0,0 +1,21 @@
+package com.youtan.leilao.DTO;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.youtan.leilao.model.TipoCombustivel;
+import com.youtan.leilao.model.TipoVeículo;
+
+import java.math.BigDecimal;
+
+public record VeiculoDTO(
+    Long id,
+    String placa,
+    String marcaModelo,
+    Integer anoFabricacao,
+    String Cor,
+    TipoCombustivel tipoCombustível,
+    TipoVeículo tipoVeículo,
+    Integer nPortas,
+    Integer qtdPassageiros,
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "#,##0.00",locale = "pt-BR")
+    BigDecimal valor
+) {}
